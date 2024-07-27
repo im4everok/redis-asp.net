31eaa49c644b106cbe894fffab989a5297b2d775
im4everok <imizyk09@gmail.com>
2024-07-27 17:39:18 +0300

    added redis client nuget package, added redis and http client to DI

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("localhost:6379"));
+builder.Services.AddHttpClient();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
